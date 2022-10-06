83c8fcd9c237d20bba507cae8c3d81d3aeabfecd
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2022-10-05 21:53:32 -0300

    first commit

---
+++ programs/cpi-token-onchain-sample/src/lib.rs
@@ -0,0 +1,58 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Transfer};
+use anchor_spl::token::{Mint, TokenAccount};
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod cpi_token_onchain_sample {
+
+    use super::*;
+
+    pub fn initialize(ctx: Context<TransferInstruction>, amount: u64, nonce: u8) -> Result<()> {
+        let seeds = &[
+            ctx.accounts.mint.to_account_info().key.as_ref(),
+            &[nonce],
+        ];
+        let signer = &[&seeds[..]];
+        msg!("check3");
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.program_signer.clone(),
+        };
+        let cpi_program = ctx.accounts.token_program.clone();
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+        msg!("check4");
+        token::transfer(cpi_ctx, amount).expect("transfer2 failed"); //?;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct TransferInstruction<'info> {
+    /// CHECK: xxx
+    pub program_signer: AccountInfo<'info>,
+
+    /// CHECK: xxx
+    #[account(signer)] //authority should sign this txn
+    pub authority: AccountInfo<'info>,
+
+    pub mint: Account<'info, Mint>,
+
+    /// CHECK: xxx
+    #[account(mut)]
+    pub to: Account<'info, TokenAccount>,
+
+    /// CHECK: xxx
+    #[account(mut)]
+    pub from: Account<'info, TokenAccount>,
+
+    /// CHECK: xxx
+    // We already know its address and that it's executable
+    #[account(executable, "token_program.key == &token::ID")]
+    pub token_program: AccountInfo<'info>,
+
+    /// CHECK: xxx
+    pub system_program: AccountInfo<'info>,
+}
---
+++ tests/cpi-token-onchain-sample.ts
@@ -0,0 +1,46 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { CpiTokenOnchainSample } from "../target/types/cpi_token_onchain_sample";
+import {
+  createAssociatedTokenAccount,
+  createMint,
+  mintTo
+} from "@solana/spl-token";
+
+describe("cpi-token-onchain-sample", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.CpiTokenOnchainSample as Program<CpiTokenOnchainSample>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const connection = anchor.getProvider().connection;
+    const fromWallet = anchor.web3.Keypair.generate();
+    const toWallet = anchor.web3.Keypair.generate();
+    const signature = await connection.requestAirdrop(fromWallet.publicKey, 1000000000);
+    await connection.confirmTransaction(signature, 'confirmed');
+    console.log({ from: fromWallet.publicKey.toBase58() })
+    const mint = await createMint(connection, fromWallet, fromWallet.publicKey, fromWallet.publicKey, 9);
+
+    const fromAccount = await createAssociatedTokenAccount(connection, fromWallet, mint, fromWallet.publicKey)
+    await mintTo(connection, fromWallet, mint, fromAccount, fromWallet, 5000000000);
+    const toAccount = await createAssociatedTokenAccount(connection, fromWallet, mint, toWallet.publicKey)
+    console.log('initializing...')
+    const tx = await program.methods.initialize(new anchor.BN(2000000000), 1)
+      .accounts({
+        mint,
+        from: fromAccount,
+        to: toAccount,
+        programSigner: fromWallet.publicKey,
+        authority: fromWallet.publicKey,
+      })
+      .signers([fromWallet, fromWallet])
+      .rpc();
+    console.log("Your transaction signature", tx);
+    const balance = await connection.getParsedTokenAccountsByOwner(
+      fromWallet.publicKey, { mint }
+    );
+    console.log(balance.value[0]?.account.data.parsed.info.tokenAmount)
+  });
+});
