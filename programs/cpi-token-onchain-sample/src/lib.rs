a1f1c7d93b99e5233ba6bb0a07261f0750acba45
Fabio Oshiro <fabio.oshiro@gmail.com>
2022-10-10 23:13:22 -0300

    spl token transfer with or without signature

--- programs/cpi-token-onchain-sample/src/lib.rs
+++ programs/cpi-token-onchain-sample/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Transfer, Mint, TokenAccount};
+use anchor_spl::token::{self, Transfer, Mint, SetAuthority, TokenAccount};
+use spl_token::instruction::AuthorityType;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -8,22 +9,61 @@ pub mod cpi_token_onchain_sample {
 
     use super::*;
 
-    pub fn initialize(ctx: Context<TransferInstruction>, amount: u64, nonce: u8) -> Result<()> {
-        let seeds = &[
-            ctx.accounts.mint.to_account_info().key.as_ref(),
-            &[nonce],
+    const WALLET_PDA_SEED: &[u8] = b"wallet";
+
+    pub fn init_wallet(ctx: Context<InitWallet>) -> Result<()> {
+
+        // take the ownership of this TokenAccount
+        let cpi_accounts = SetAuthority {
+            account_or_mint: ctx.accounts.escrow_wallet.to_account_info(),
+            current_authority: ctx.accounts.user_sending.to_account_info(),
+        };
+        let cpi_context = CpiContext::new(ctx.accounts.token_program.clone(), cpi_accounts);
+        let (vault_authority, _bump) =
+            Pubkey::find_program_address(&[
+                WALLET_PDA_SEED,
+                ctx.accounts.mint.to_account_info().key.as_ref()
+            ], ctx.program_id);
+        token::set_authority(
+            cpi_context,
+            AuthorityType::AccountOwner,
+            Some(vault_authority),
+        )?;
+        Ok(())
+    }
+
+    pub fn wallet_transfer(ctx: Context<WalletTransfer>, amount: u64) -> Result<()> {
+        let (_vault_authority, vault_authority_bump) =
+            Pubkey::find_program_address(&[
+                WALLET_PDA_SEED,
+                ctx.accounts.mint.to_account_info().key.as_ref()
+            ], ctx.program_id);
+        let authority_seeds = &[
+            &WALLET_PDA_SEED[..],
+            ctx.accounts.mint.to_account_info().key.as_ref(), 
+            &[vault_authority_bump]
         ];
-        let signer = &[&seeds[..]];
+        let signer = &[&authority_seeds[..]];
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.escrow_wallet.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.authority.clone(),
+        };
+        let cpi_program = ctx.accounts.token_program.clone();
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+        token::transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
+
+    pub fn transfer(ctx: Context<TransferInstruction>, amount: u64) -> Result<()> {
         let cpi_accounts = Transfer {
             from: ctx.accounts.from.to_account_info(),
             to: ctx.accounts.to.to_account_info(),
             authority: ctx.accounts.program_signer.clone(),
         };
         let cpi_program = ctx.accounts.token_program.clone();
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
-        msg!("Calling transfer...");
-        token::transfer(cpi_ctx, amount).expect("transfer2 failed"); //?;
-        msg!("Transfer success.");
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::transfer(cpi_ctx, amount)?;
         Ok(())
     }
 }
@@ -47,9 +87,64 @@ pub struct TransferInstruction<'info> {
     #[account(mut)]
     pub from: Account<'info, TokenAccount>,
 
+    /// CHECK: We already know its address and that it's executable
+    #[account(executable, constraint = token_program.key == &token::ID)]
+    pub token_program: AccountInfo<'info>,
+
     /// CHECK: xxx
-    // We already know its address and that it's executable
-    #[account(executable, "token_program.key == &token::ID")]
+    pub system_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct InitWallet<'info> {
+
+    #[account(
+        init,
+        payer = user_sending,
+        seeds=[
+            b"wallet".as_ref(),
+            mint.key().as_ref()
+        ],
+        bump,
+        token::mint=mint,
+        token::authority=user_sending,
+    )]
+    escrow_wallet: Account<'info, TokenAccount>,
+
+    // Users and accounts in the system
+    #[account(mut)]
+    user_sending: Signer<'info>, // Alice
+    mint: Account<'info, Mint>,  // USDC
+
+    /// CHECK: We already know its address and that it's executable
+    #[account(executable, constraint = token_program.key == &token::ID)]
+    pub token_program: AccountInfo<'info>,
+
+    /// CHECK: xxx
+    pub system_program: AccountInfo<'info>,
+
+    rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct WalletTransfer<'info> {
+    #[account(mut, signer)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub taker: AccountInfo<'info>,
+
+    #[account(mut)]
+    escrow_wallet: Account<'info, TokenAccount>,
+    mint: Account<'info, Mint>,  // USDC
+
+    /// CHECK: xxx
+    pub authority: AccountInfo<'info>,
+
+    /// CHECK: xxx
+    #[account(mut)]
+    pub to: Account<'info, TokenAccount>,
+
+    /// CHECK: We already know its address and that it's executable
+    #[account(executable, constraint = token_program.key == &token::ID)]
     pub token_program: AccountInfo<'info>,
 
     /// CHECK: xxx
--- tests/cpi-token-onchain-sample.ts
+++ tests/cpi-token-onchain-sample.ts
@@ -6,28 +6,89 @@ import {
   createMint,
   mintTo
 } from "@solana/spl-token";
+import { PublicKey, SystemProgram, Transaction, Connection, Commitment } from '@solana/web3.js';
+import { expect } from "chai";
+
 
 describe("cpi-token-onchain-sample", () => {
+  const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.CpiTokenOnchainSample as Program<CpiTokenOnchainSample>;
+  const connection = anchor.getProvider().connection;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const connection = anchor.getProvider().connection;
+  it("should initialize the pda token account", async () => {
     const fromWallet = anchor.web3.Keypair.generate();
     const toWallet = anchor.web3.Keypair.generate();
-    const signature = await connection.requestAirdrop(fromWallet.publicKey, 1000000000);
+    const signature = await connection.requestAirdrop(fromWallet.publicKey, 1_000_000_000);
     await connection.confirmTransaction(signature, 'confirmed');
-    console.log({ from: fromWallet.publicKey.toBase58() })
+    const mint = await createMint(connection, fromWallet, fromWallet.publicKey, fromWallet.publicKey, 9);
+
+    const [escrowWallet, bump] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from(anchor.utils.bytes.utf8.encode("wallet")),
+        mint.toBuffer(),
+      ],
+      program.programId
+    );
+    const userReceiving = toWallet.publicKey;
+    console.log('Init wallet...', {
+      from: fromWallet.publicKey.toBase58(),
+      escrowWallet: escrowWallet.toBase58(),
+      bump
+    });
+    await program.methods
+      .initWallet()
+      .accounts({
+        escrowWallet,
+        mint,
+        userSending: fromWallet.publicKey,
+      })
+      .signers([fromWallet])
+      .rpc();
+    console.log('minting to...');
+    await mintTo(connection, fromWallet, mint, escrowWallet, fromWallet, 5_000_000_000);
+    const accountInfo = await connection.getParsedAccountInfo(escrowWallet);
+    console.log(JSON.stringify(accountInfo.value.data, null, 4));
+    const toAccount = await createAssociatedTokenAccount(connection, fromWallet, mint, toWallet.publicKey)
+    
+    console.log('wallet transfer', toAccount.toBase58());
+    await program.methods
+      .walletTransfer(new anchor.BN(1_500_000_000))
+      .accounts({
+        escrowWallet,
+        to: toAccount,
+        taker: userReceiving,
+        authority: escrowWallet,
+        mint,
+        // tokenProgram: escrowWallet,
+      })
+      .signers([toWallet])
+      .rpc();
+
+    const accountInfo2 = await connection.getParsedAccountInfo(escrowWallet);
+    console.log(JSON.stringify(accountInfo2.value.data, null, 4));
+    expect(+(accountInfo2.value.data as any).parsed.info.tokenAmount.amount).to.eq(3_500_000_000);
+  })
+
+  it("should do p2p transfer", async () => {
+    const fromWallet = anchor.web3.Keypair.generate();
+    const toWallet = anchor.web3.Keypair.generate();
+    const signature = await connection.requestAirdrop(fromWallet.publicKey, 1_000_000_000);
+    await connection.confirmTransaction(signature, 'confirmed');
+    console.log({ from: fromWallet.publicKey.toBase58() });
     const mint = await createMint(connection, fromWallet, fromWallet.publicKey, fromWallet.publicKey, 9);
 
     const fromAccount = await createAssociatedTokenAccount(connection, fromWallet, mint, fromWallet.publicKey)
-    await mintTo(connection, fromWallet, mint, fromAccount, fromWallet, 5000000000);
+    await mintTo(connection, fromWallet, mint, fromAccount, fromWallet, 5_000_000_000);
     const toAccount = await createAssociatedTokenAccount(connection, fromWallet, mint, toWallet.publicKey)
-    console.log('Call contract...')
-    const tx = await program.methods.initialize(new anchor.BN(2000000000), 1)
+
+    // wait for the next block
+    // await delay(1);
+    console.log('Call "simple" transfer...');
+    const tx = await program.methods.transfer(new anchor.BN(2_000_000_000))
       .accounts({
         mint,
         from: fromAccount,
@@ -35,12 +96,13 @@ describe("cpi-token-onchain-sample", () => {
         programSigner: fromWallet.publicKey,
         authority: fromWallet.publicKey,
       })
-      .signers([fromWallet, fromWallet])
+      .signers([fromWallet])
       .rpc();
     console.log("Your transaction signature", tx);
     const balance = await connection.getParsedTokenAccountsByOwner(
       fromWallet.publicKey, { mint }
     );
     console.log(balance.value[0]?.account.data.parsed.info.tokenAmount)
+    expect(+balance.value[0]?.account.data.parsed.info.tokenAmount.amount).to.eq(3_000_000_000);
   });
 });
